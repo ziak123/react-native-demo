03bd5756dd008c21576d22552aa0a48ebf6bb781
ziakhanmubbits <ziakhansoftwaredeveloper1@gmail.com>
2023-09-11 13:42:46 +0500

    e
    e

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Appsjsjs</Text>
+      <Text>kkddk</Text>
+    </View>
+  )
+}
+
+export default App
